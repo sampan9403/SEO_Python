cf00eb5564189d28d95361d6f2e0b74608526f0c
sampan9403 <119164347+sampan9403@users.noreply.github.com>
2023-12-12 17:36:26 +0800

    Update keywords_serp_serpapi_100.py
    -Del language and amend google_domain to google.com

--- keywords_serp/keywords_serp_serpapi_100.py
+++ keywords_serp/keywords_serp_serpapi_100.py
@@ -60,8 +60,7 @@ keywords = keywords_df["keywords"].tolist()
 # SERPAPI parameters common to all searches
 base_params = {
     "location": "Hong Kong",
-    "google_domain": "google.com.hk",
-    "hl": "en",
+    "google_domain": "google.com",
     "gl": "hk",
     "api_key": "7ec738d5269f13638e91542f7419b4122c614ac0e82fcc4ee5afbfd072f910cb",
     "num": 100  # Adjust this number as needed
